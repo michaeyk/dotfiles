219241ed5459ef6c08db1cf5a5b109e5a1703e77
Michael Kim <michaeyk@gmail.com>
2022-10-02 08:01:46 -0600

    Don't scroll items with tab in cmp

--- dotconfig/nvim/lua/user/cmp.lua
+++ dotconfig/nvim/lua/user/cmp.lua
@@ -66,9 +66,9 @@ cmp.setup {
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = false },
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
+      -- if cmp.visible() then
+      --   cmp.select_next_item()
+      if luasnip.expandable() then
         luasnip.expand()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
@@ -82,9 +82,9 @@ cmp.setup {
       "s",
     }),
     ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      -- if cmp.visible() then
+      --   cmp.select_prev_item()
+      if luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
